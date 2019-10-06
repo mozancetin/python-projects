90605b7b28a8a54d6d608cddcfe7171b0db452bb
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-06 23:46:36 +0300

    Let the computer hold a number and you guess.
    Guess game.

---
+++ tahminedilensayibul.py
@@ -0,0 +1,16 @@
+import random
+n=random.randint(1,100)
+c=1
+while(True):
+    tahmin=int(input("Tahmin et:"))
+    if tahmin==n:
+        print("Aklımdan tuttuğum sayı:",n)
+        print("Tebrikler",c,"adımda buldunuz!")
+        break
+    elif tahmin>n:
+        print("Daha küçük bir sayı söylemelisin")
+        c=c+1
+    else:
+        print("Daha büyük bir sayı söylemelisin")
+        c=c+1
+input()
