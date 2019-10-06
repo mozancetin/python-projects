3f32f5a3d4edbfea8efa83328c83964c02a8b972
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-06 23:27:56 +0300

    Upgraded PC Guess

---
+++ pcguessupgraded.py
@@ -0,0 +1,18 @@
+import random
+c=1
+a=1
+b=100
+while(True):
+    tahmin=int((a+b)/2)
+    print("\n" , "Tahminim:" , tahmin , "doğru mu? \n")
+    bkmk=int(input(" Daha büyükse: 1 \n Daha küçükse: -1 \n Bildiyse:0 yazın \n "))
+    if bkmk==0:
+        print(c,"adımda bildim!")
+        break
+    elif bkmk==1:
+        a=tahmin
+        c=c+1
+    elif bkmk==-1:
+        b=tahmin
+        c=c+1
+input()
