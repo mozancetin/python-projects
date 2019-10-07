32756c6d2ce6c9d22abf9d7d935836f443f8d72f
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-07 19:51:39 +0300

    Bug Fixed

--- 002-asalmidegilmi.py
+++ 002-asalmidegilmi.py
@@ -1,15 +1,19 @@
-n=int(input("\nAsal sayı olup olumadığını sorgulamak istediğiniz sayıyı girin: "))
+n=int(input("Asal sayı olup olumadığını sorgulamak istediğiniz sayıyı girin: "))
 i=2
 while(True):
-    if n<i**2 :
-        print("Asal sayı")
-        break
-    if int(n/i)*i==n :
+    if n==0 or n==1:
         print("Asal sayı değil")
         break
     else:
-        if int(i/2)*2==i :
-            i=i+1
+        if n<i**2 :
+            print("Asal sayı")
+            break
+        if int(n/i)*i==n :
+            print("Asal sayı değil")
+            break
         else:
-            i=i+2
+            if int(i/2)*2==i :
+                i=i+1
+            else:
+                i=i+2
 input()
