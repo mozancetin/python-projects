fc0e71f4f2490430d1c71d6aa390bf6d9bb1a93e
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-14 19:57:27 +0300

    İtems added

--- siralisavas.py
+++ siralisavas.py
@@ -3,14 +3,31 @@ import random
 rcan=10
 can=10
 t=1
+item=[1,1,1] #kılıç(fazla vurma) , kalkan(hasar azaltma) , kıyafet(can) 
+
+a=1
+b=5
+
+c=1
+d=5
+
+if item[0]==1:
+    a=3
+    b=7
+if item[2]==1:
+    can=can+4
+
+
 while(True):
     print("\n"*50)
-    attack=random.randint(1,5)
-    rattack=random.randint(1,5)
+    attack=random.randint(a,b)
+    rattack=random.randint(c,d)
+    if item[1]==1:
+        rattack=rattack-1
     
     if (t%2) == 1:
         if attack==1:
-            print("Saldırı yapılıyor...")
+            print("[ Tur",t,"]","Saldırı yapılıyor...")
             time.sleep(1)
             print("Weak!",attack)
             rcan=rcan-attack
@@ -21,7 +38,7 @@ while(True):
             time.sleep(2)
             t+=1
         elif attack==2:
-            print("Saldırı yapılıyor...")
+            print("[ Tur",t,"]","Saldırı yapılıyor...")
             time.sleep(1)
             print("Normal!",attack)
             rcan=rcan-attack
@@ -32,7 +49,7 @@ while(True):
             t+=1
             time.sleep(2)
         elif attack==3:
-            print("Saldırı yapılıyor...")
+            print("[ Tur",t,"]","Saldırı yapılıyor...")
             time.sleep(1)
             print("Strong!",attack)
             rcan=rcan-attack
@@ -43,7 +60,7 @@ while(True):
             t+=1
             time.sleep(2)
         else:
-            print("Saldırı yapılıyor...")
+            print("[ Tur",t,"]","Saldırı yapılıyor...")
             time.sleep(1)
             print("Too Strong!",attack)
             rcan=rcan-attack
@@ -57,7 +74,7 @@ while(True):
         
     elif (t%2) == 0:
         if rattack==1:
-            print("Rakip saldırı yapıyor...")
+            print("[ Tur",t,"]","Rakip saldırı yapıyor...")
             time.sleep(1)
             print("Weak!",rattack)
             can=can-rattack
@@ -68,7 +85,7 @@ while(True):
             t+=1
             time.sleep(2)
         elif rattack==2:
-            print("Rakip saldırı yapıyor...")
+            print("[ Tur",t,"]","Rakip saldırı yapıyor...")
             time.sleep(1)
             print("Normal!",rattack)
             can=can-rattack
@@ -79,7 +96,7 @@ while(True):
             t+=1
             time.sleep(2)
         elif rattack==3:
-            print("Rakip saldırı yapıyor...")
+            print("[ Tur",t,"]","Rakip saldırı yapıyor...")
             time.sleep(1)
             print("Strong!",rattack)
             can=can-rattack
@@ -90,7 +107,7 @@ while(True):
             t+=1
             time.sleep(2)
         else:
-            print("Rakip saldırı yapıyor...")
+            print("[ Tur",t,"]","Rakip saldırı yapıyor...")
             time.sleep(1)
             print("Too Strong!",rattack)
             can=can-rattack
