79ade4d9e698cd1e64e652df5aa9b4804532c073
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-16 22:53:49 +0300

    Add files via upload

---
+++ ProjectEuler/001-3ve5inkatlarıtoplamı.py
@@ -0,0 +1,11 @@
+#1000'den küçük 3 ve 5'in katlarının toplamı
+toplam=0
+i=1
+while(i<1000):
+    if i%3==0:
+        toplam+=i
+    elif i%5==0:
+        toplam+=i
+    i+=1
+print(toplam)
+input()
---
+++ ProjectEuler/002-ciftfibonacci.py
@@ -0,0 +1,24 @@
+import time
+fib1=1
+fib2=1
+i=3
+t=0
+n=int(input("Hangi sayıdan küçük tüm çift fibonacci sayılarının toplamını istersiniz?\n"))
+time.sleep(0.2)
+while(True):
+    fib=fib1+fib2
+    if int(fib/2)*2==int(fib):
+        """
+        print("Çift sayı",fib)
+        çift sayıları bastırmak isterseniz
+        """
+        t=t+fib
+
+    if fib>n:
+        print("\n",n,"sayısından küçük tüm çift fibonacci sayılarının toplamı:",t)
+        break
+    else:
+        fib1=fib2
+        fib2=fib
+        i=i+1
+input("\n Çıkmak için enter'a basın...")
