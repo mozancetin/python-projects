54340159772076547209277be47893cb3d64a9f2
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-25 23:26:28 +0300

    A few corrections were made.

--- Other Projects/006-tahminedilensayibul.py
+++ Other Projects/006-tahminedilensayibul.py
@@ -3,15 +3,15 @@ import random
 n=random.randint(1,100)
 c=1
 while(True):
-    tahmin=int(input("Tahmin et:"))
+    tahmin=int(input("Tahmin et: "))
     if tahmin==n:
-        print("Aklımdan tuttuğum sayı:",n)
+        print("Aklımdan tuttuğum sayı: ",n)
         print("Tebrikler",c,"adımda buldunuz!")
         break
     elif tahmin>n:
-        print("Daha küçük bir sayı söylemelisin")
+        print("Daha küçük bir sayı söylemelisin\n")
         c=c+1
     else:
-        print("Daha büyük bir sayı söylemelisin")
+        print("Daha büyük bir sayı söylemelisin\n")
         c=c+1
 input()
