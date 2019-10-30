ec9256e714b35a5b2ad582ad6f0306a343f04d32
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-10-30 18:48:18 +0300

    Problem 5 Eklendi

---
+++ ProjectEuler/005-smallest multiple.py
@@ -0,0 +1,16 @@
+a = 1
+c = 0
+i = 1
+while(c<20):
+     if a%i == 0 and a%2520 == 0:
+        c += 1
+        i += 1
+     else:
+        i = 1
+        a += 1
+        c = 0
+print(a)
+input()
+        
+        
+    
