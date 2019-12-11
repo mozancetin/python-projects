eb12af6033bdfa4bc7828e998002dbf1832c4aef
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2019-12-11 20:53:14 +0300

    Update 012-Highly divisible triangular number.py

--- ProjectEuler/012-Highly divisible triangular number.py
+++ ProjectEuler/012-Highly divisible triangular number.py
@@ -1,8 +1,18 @@
-from fonksiyon import triangle
-from fonksiyon import bolenler
-#Functions are in my repository
-n = 1
-while(bolenler(triangle(n))<500):
-    n += 1
+def bolenler(n):
+    carpanlar = [1]
+    q = int(n**(1/2))
+    for p in range(1,q+1):
+        if (n%p == 0):
+            carpanlar.append(p)
+            carpanlar.append(int(n/p))
+    if (q**2==n):
+        return(carpanlar[:-1])
+    return(carpanlar)
 
-input()
+n, sayi = 1, 1
+
+while(len(bolenler(sayi))<500):
+    n += 1
+    sayi += n
+
+print(sayi)
