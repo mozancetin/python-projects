027667e2aad72570ccdc8a09cadb810ed06aaa21
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2020-05-27 19:37:43 +0300

    Add files via upload

---
+++ Other Projects/matris çarpımı.py
@@ -0,0 +1,45 @@
+#matris çarpımı
+
+print("Matrislerin boyutlarını girin ( A(mxn) , B(nxp) )")
+
+m = int(input("m: "))
+n = int(input("n: "))
+p = int(input("p: "))
+
+A = []
+B = []
+C = []
+
+print("\n")
+for i in range(0,m):
+    A.append([])
+    for x in range(0,n):
+        sayi = int(input("A[{}x{}] sayısını girin: ".format(i+1,x+1)))
+        A[i].append(sayi)
+
+print("\n")
+print("A matrisi: ", A)
+input("B matrisini girmek için enter'a basın...")
+print(3*"\n")
+for i in range(0,n):
+    B.append([])
+    for x in range(0,p):
+        sayi = int(input("B[{}x{}] sayısını girin: ".format(i+1,x+1)))
+        B[i].append(sayi)
+
+print("\n")
+print("A matrisi: ", A)
+print("B matrisi: ", B)
+print(5*"\n")
+
+def carpma():
+    
+    import numpy as np
+
+    Q = np.array([A])
+    E = np.array([B])
+    C = Q.dot(E)
+    print(C[0])
+
+carpma()
+input()
