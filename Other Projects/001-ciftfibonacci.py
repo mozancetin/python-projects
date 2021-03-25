45a4651e32e22ecb6903c086f7c0ca1608320458
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2021-03-25 16:21:01 +0300

    Update 001-ciftfibonacci.py

--- Other Projects/001-ciftfibonacci.py
+++ Other Projects/001-ciftfibonacci.py
@@ -2,7 +2,6 @@
 import time
 fib1=1
 fib2=1
-i=3
 t=0
 n=int(input("Hangi sayıdan küçük tüm çift fibonacci sayılarının toplamını istersiniz?\n"))
 time.sleep(0.2)
@@ -21,5 +20,4 @@ while(True):
     else:
         fib1=fib2
         fib2=fib
-        i=i+1
 input("\n Çıkmak için enter'a basın...")
