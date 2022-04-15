927b68006d62c61764f21124edcc4b27ddcc639e
Mustafa Ozan Çetin <53947781+mozancetin@users.noreply.github.com>
2022-04-16 02:47:50 +0300

    Update 016-powerdigitsum.py

--- ProjectEuler/016-powerdigitsum.py
+++ ProjectEuler/016-powerdigitsum.py
@@ -1,9 +1,2 @@
 #2^1000 sayısının basamakları toplamı
-n = 2**1000
-toplam = 0
-while(n>=10):
-    toplam += n % 10
-    n = n // 10
-toplam += n
-print("2^1000 sayısının basamakları toplamı: {}".format(toplam))
-input()
+print(sum(list(map(int, list(str(2**1000))))))
